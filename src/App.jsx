f1435f8be584e3ddb309b2c0273d710652c2bd9a
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-09-24 22:09:55 -0300

    importing some stuff on app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import './App.css'
-import React from 'react'
+import React, { useEffect } from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { tooltipComponent } from '@syncfusion/ej2-react-popups';
 
 const App = () => {
   return (
